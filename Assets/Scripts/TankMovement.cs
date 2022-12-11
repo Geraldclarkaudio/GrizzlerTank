045134ca733c529ffbbad8808498ca717665b759
Gerald <geraldclarkaudio@gmail.com>
2022-12-11 15:57:38 -0600

    added new project

---
+++ Assets/Scripts/TankMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
