415b698ccdeea1e67098d42e041f25e420e9808c
Gerald <geraldclarkaudio@gmail.com>
2022-12-12 00:54:54 -0600

    added some tank movement functionality and connected wwise rtpcs to proper variables

--- Assets/Scripts/TankMovement.cs
+++ Assets/Scripts/TankMovement.cs
@@ -4,18 +4,85 @@ using UnityEngine;
 
 public class TankMovement : MonoBehaviour
 {
-    [SerializeField]
+    [Header("Tank Specific")]
+    [SerializeField] [Range(0, 25)]
     private float _speed;
+    [SerializeField] [Range(0,1)]
+    private float isMoving = 0.5f;
+
+    [SerializeField]
+    private float movementSpeed;
+
+    [Header("Wwise Events")]
+    [SerializeField]
+    private AK.Wwise.Event grizzlerEvent;
+    [SerializeField]
+    private AK.Wwise.Event beginMovementEvent;
+    [SerializeField]
+    private AK.Wwise.Event stopMovementEvent;
+    [Header("RTPCs")]
+    [SerializeField]
+    private AK.Wwise.RTPC playerSpeedRTPC;
+    [SerializeField]
+    private AK.Wwise.RTPC playerLoadRTPC;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        grizzlerEvent.Post(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        playerSpeedRTPC.SetValue(gameObject, _speed);
+        playerLoadRTPC.SetValue(gameObject, isMoving);
+
+        float verticalInput = Mathf.Abs(Input.GetAxisRaw("Vertical"));
+        float horizontalInput = Mathf.Abs(Input.GetAxisRaw("Horizontal"));
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+
+        if(verticalInput == 1 || horizontalInput == 1) // if Holding an input
+        {
+            isMoving += 0.1f * Time.deltaTime; // set load
+         
+            _speed += movementSpeed * Time.deltaTime; // increase speed over time. 
+
+            if(isMoving >= 1)
+            {
+                isMoving = 1;
+            }
+
+            if(_speed >= 25)
+            {
+                _speed = 25; //cap speed at 25
+            }
         
+            transform.Translate(direction * _speed * Time.deltaTime); // move object in whatever direction at speed of _speed;
+        }
+
+        else if(verticalInput == 0 || horizontalInput == 0) // if not holding input
+        {
+            _speed -= movementSpeed * Time.deltaTime; // decrease speed over time
+
+            if (isMoving <= 0)
+            {
+                isMoving = 0;
+            }
+
+            if (_speed > 0)
+            {
+                isMoving -= 0.1f * Time.deltaTime; // cap at 0
+            }
+
+            if(_speed <= 0)
+            {
+                isMoving = 0.5f; // set load back to 0.5 when reaching 0 _speed;
+                _speed = 0;
+            }
+
+            transform.Translate(new Vector3(0,0,0.5f) * _speed * Time.deltaTime);
+        }
     }
+
+
 }
---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,101 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BEGINMOVING = 3807729250U;
+        static const AkUniqueID GRIZZLERMOVEMENT = 3315242463U;
+        static const AkUniqueID STOPMOVING = 1580062387U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace PLAYERMOVEMENT
+        {
+            static const AkUniqueID GROUP = 1350496757U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ISIDLE = 2139983939U;
+                static const AkUniqueID ISMOVING = 3940648127U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace PLAYERMOVEMENT
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace SURFACETYPE
+        {
+            static const AkUniqueID GROUP = 63790334U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CONCRETE = 841620460U;
+                static const AkUniqueID METAL = 2473969246U;
+            } // namespace SWITCH
+        } // namespace SURFACETYPE
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID DENSITY_FA_SSGRAIN = 2715217995U;
+        static const AkUniqueID IGN_ENG_EXH_POSITION = 1776289502U;
+        static const AkUniqueID IGN_GEARBOX_THROTTLE = 3744760799U;
+        static const AkUniqueID IGN_HARLEY_GRAIN_LOOP_BLEND = 1573260114U;
+        static const AkUniqueID IGN_LOAD = 1046277832U;
+        static const AkUniqueID IGN_RPM = 2202868459U;
+        static const AkUniqueID IGNITERSYNTH_REV = 1355046805U;
+        static const AkUniqueID IGNITERSYNTH_STATE = 3696491809U;
+        static const AkUniqueID IMMERSION_FA_SSGRAIN = 2481728872U;
+        static const AkUniqueID INSTRUMENT_FA_SSGRAIN = 2317409760U;
+        static const AkUniqueID PLAYER_LOAD = 3610741985U;
+        static const AkUniqueID PLAYERSPEED = 1493153371U;
+        static const AkUniqueID PROXIMITY_FA_SSGRAIN = 1791284502U;
+        static const AkUniqueID RPM_FA_SSGRAIN = 1656280998U;
+        static const AkUniqueID SIMULATION_FA_SSGRAIN = 2428833394U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+        static const AkUniqueID WHEELSURFACE = 1446210263U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID GRIZZLER = 1291779240U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
